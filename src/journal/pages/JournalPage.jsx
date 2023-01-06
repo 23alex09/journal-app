b12bc6da964dd1131b73bff6da7330865e6b7539
Alejandro Hidalgo Acuña <23alex09@gmail.com>
2023-01-06 21:30:57 +0100

    add image gallery and floating button to add more notes

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { AddOutlined } from "@mui/icons-material"
+import { IconButton } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -7,8 +9,22 @@ export const JournalPage = () => {
 
             {/* El typografy si acaba traduciendo en un elemento html en este caso el indicado en la propiedad variant, tambien lo podemos especificar en la propiedad component pero este no cambiara su apariencia en pantalla */ }
             {/* <Typography variant='h1'>JournalPage</Typography> */ }
-            {/* <NothingSelectedView/> */ }
-            <NoteView />
+            <NothingSelectedView />
+            {/* <NoteView /> */ }
+            <IconButton
+                size='large'
+                sx={ {
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                } }
+            >
+                <AddOutlined sx={ { fontSize: 30 } } />
+            </IconButton>
+
         </JournalLayout>
     )
 }
